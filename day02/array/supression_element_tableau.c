978ab1f7ca28858fc0e8ee86b815b110d8815f63
otmane <elkorriotmane@gmail.com>
2024-09-28 17:14:26 +0200

    elkorri

---
+++ day02/array/supression_element_tableau.c
@@ -0,0 +1,27 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   
+   int T[5]= {1,2,3,4,5};
+   int remove = 1 , size = sizeof(T) / sizeof(T[0]);
+   int j , i ;
+   
+   for (int i = 0;i<5;i++){
+       if (T[i]==2){
+           remove = i ;
+           break;
+       }
+   }
+   for ( j=remove;j<5-1;j++){
+       T[j] = T[j+1];
+   }
+   size --;
+   for (int i = 0;i<5-1;i++){
+       printf("%d",T[i]);
+   }
+   
+   
+
+    return 0;
+}
