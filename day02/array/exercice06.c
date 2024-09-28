859c181849bc2941748e4ae6335cd5260b689d2b
otmane <elkorriotmane@gmail.com>
2024-09-28 17:11:26 +0200

    elkorri

---
+++ day02/array/exercice06.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a , b , i ,rs =1 ;
+    
+    printf("saisir combien d'element tu veux dans le tableau :");
+    scanf("%d",&a);
+     int table[a];
+     for(i=0;i<a;i++){
+         
+         printf("saisir votre element sltp : ");
+         scanf("%d",&table[b]);
+         
+         
+      rs =  rs * table[b] ;
+     }
+     
+     printf("%d",rs);
+     
+    
+    
+    
+    return 0;
+}
+  
